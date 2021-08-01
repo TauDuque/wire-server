e34462ecf2794161ec616404f10fcf48d55cb606
TauDuque <hectauduque@gmail.com>
2021-07-31 23:50:37 -0300

    credential

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
   port: 3306,
-  database: "wire-db",
+  database: "heroku_3a959d1095bb563",
   define: {
     timestamps: false,
     underscored: true,
